eb37bae82f31d4e5aa64760b48940255610504b1
gelebus <61007922+gelebus@users.noreply.github.com>
2021-03-24 11:44:37 +0100

    Create AdminController.cs

---
+++ Webshop2021/Webshop2021/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webshop2021.Controllers
+{
+    [Route("[controller]")]
+    public class AdminController : Controller
+    {
+        [HttpPost("products")]
+        public IActionResult CreateProduct()
+        {
+            //api
+            return View();
+        }
+    }
+}
